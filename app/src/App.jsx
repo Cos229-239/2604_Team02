8368d5571486e5153f47938fea99fcd40c70b27b
crcatoruiz <chrisruiz1121@att.net>
2026-04-30 19:51:47 -0700

    Tea API card data import and cost sorting

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,12 @@
-import cards from "./data/cards.json";
-import Card from "./components/Card";
+import Cards2 from "./components/Cards2";
 
 function App() {
-  return (
+return (
     <div style={{ padding: "20px" }}>
       <h1>Spire Architect</h1>
       <p>Slay the Spire Companion App</p>
 
-      <h2>Card List</h2>
-
-      {cards.map((card) => (
-        <Card key={card.id} card={card} />
-      ))}
+      <Cards2 />
     </div>
   );
 }
